7888b9534520bce511193f0aefe14eed5401b8a7
Stetson Ramey <stetson.ramey@gmail.com>
2018-03-07 08:20:47 -0600

    working the yelp API pull

---
+++ assets/javascript/logic.js
@@ -0,0 +1,13 @@
+var city;
+var state;
+var country;
+var location = city + "+" + state + "+" + country;
+var category;
+
+var yelpKey = "QMBv4dcuLUi8EcphMbSWYDxIisioJ7Sy1NuC7dCrtWuVygMheahI3UwcrFY7Px5Mb_sa7t6LR0oik5JF1iV4nRpDXpsBgg4IDXH59SDSutgagnvXbtWZVTIFAu-fWnYx";
+
+
+var baseURl = "https://api.yelp.com/v3/businesses/search?term=";
+
+
+city

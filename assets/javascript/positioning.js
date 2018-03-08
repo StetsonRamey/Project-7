0f01d444948da0f8b9931b8f5948bb4bcc8435d6
Stetson Ramey <stetson.ramey@gmail.com>
2018-03-07 21:01:04 -0600

    got map to work

--- assets/javascript/positioning.js
+++ assets/javascript/positioning.js
@@ -6,25 +6,60 @@ locationForm.addEventListener("submit", geocode);
 
 function geocode(e) {
 
-    // Prevent actual submit
-    e.preventDefault();
+  // Prevent actual submit
+  e.preventDefault();
 
-    var location = document.getElementById("dest-input").value;
+  var location = document.getElementById("dest-input").value;
 
-    axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
-        params: {
-            address: location,
-            key: "AIzaSyCVv-LZUkuQRgMEx1vpNqhMnDY7LZaWnik"
-        }
-    })
-        .then(function (response) {
+  axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
+    params: {
+      address: location,
+      key: "AIzaSyCVv-LZUkuQRgMEx1vpNqhMnDY7LZaWnik"
+    }
+  })
 
-            // Log full response
-            console.log(response);
+  .then(function(response) {
 
-            // Geometry
-            var lat = response.data.results[0].geometry.location.lat;
-            var lng = response.data.results[0].geometry.location.lng;
-            console.log("LATITUDE: " + lat + "LONGITUDE: " + lng);
-        })
+    // Log full response
+    console.log(response);
+
+    // Geometry
+    var lat = response.data.results[0].geometry.location.lat;
+    var lng = response.data.results[0].geometry.location.lng;
+    console.log("LATITUDE: " + lat + "LONGITUDE: " + lng);
+
+
+    // change to the results page
+    window.location.href = 'results.html' + "?lat=" + lat + "&lng=" + lng;
+
+  })
+
+}
+
+// place a map on the screen
+
+function initMap() {
+  // change to the results page
+  var resultsURl = window.location.href;
+  console.log(resultsURl);
+
+  var params = resultsURl.split("&");
+  var lat = parseFloat(params[0].split("=")[1]);
+  var lng = parseFloat(params[1].split("=")[1]);
+  console.log(lat);
+  console.log(lng);
+
+
+  var location = {
+    lat: lat,
+    lng: lng
+  };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: location
+  });
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
 }

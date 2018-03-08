82d672e62a8badad97f02e72b95792a525ba8f5a
Stetson Ramey <stetson.ramey@gmail.com>
2018-03-07 19:00:02 -0600

    added ben's results page stuff

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -8,3 +8,33 @@ var yelpKey = "QMBv4dcuLUi8EcphMbSWYDxIisioJ7Sy1NuC7dCrtWuVygMheahI3UwcrFY7Px5Mb
 
 
 var baseURl = "https://api.yelp.com/v3/businesses/search?term=";
+
+
+// BEN'S SICK RESULTS PAGE JAVASCRIPT MAP ==========================================================
+function initMap() {
+  var myLatLng = { lat: 40.725118, lng: -73.997699 };
+
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 8,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    animation: google.maps.Animation.DROP,
+    title: "New York City"
+  });
+
+  marker.addListener("click", toggleBounce);
+
+  function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+  }
+}
+
+// END BEN'S SICK RESULTS PAGE JAVASCRIPT MAP ========================================================

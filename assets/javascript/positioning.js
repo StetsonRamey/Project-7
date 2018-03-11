fc6676b9ba9c57a3eea6a5531a1d1b0413b7637e
Stetson <stetson.ramey@gmail.com>
2018-03-11 09:52:02 -0500

    Place names populated in todo list and got the todo-list functioning (#13)
    lance's functioning todo list

--- assets/javascript/positioning.js
+++ assets/javascript/positioning.js
@@ -62,6 +62,73 @@ function callback(results, status) {
             markers.push(createMarker(results[i]));
         }
     }
+
+    //=============STETSON TRYING TO GET TODO LIST TO POPULATE==========
+    // Got the todo list to populate. Moved all js from todo-list.js inside
+    // this callback function and got it to work.
+
+    console.log(results);
+
+    // for loop to populate todo list with result name
+    for (var i = 0; i < results.length; i++) {
+        $("#myUL").append("<li>" + results[i].name + "<span class='close'>" + "\u00D7" + "</span>" + "</li>");
+    }
+
+    // Create a "close" button and append it to each list item
+    var myNodelist = document.getElementsByTagName("LI");
+    var i;
+    for (i = 0; i < myNodelist.length; i++) {
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        myNodelist[i].appendChild(span);
+    }
+
+    // Click on a close button to hide the current list item
+    var close = document.getElementsByClassName("close");
+    var i;
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+
+    // Add a "checked" symbol when clicking on a list item
+    var list = document.querySelector('ul');
+    list.addEventListener('click', function (ev) {
+        if (ev.target.tagName === 'LI') {
+            ev.target.classList.toggle('checked');
+        }
+    }, false);
+
+    // Create a new list item when clicking on the "Add" button
+    function newElement() {
+        var li = document.createElement("li");
+        var inputValue = document.getElementById("myInput").value;
+        var t = document.createTextNode(inputValue);
+        li.appendChild(t);
+        if (inputValue === '') {
+            alert("You must write something!");
+        } else {
+            document.getElementById("myUL").appendChild(li);
+        }
+        document.getElementById("myInput").value = "";
+
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        li.appendChild(span);
+
+        for (i = 0; i < close.length; i++) {
+            close[i].onclick = function () {
+                var div = this.parentElement;
+                div.style.display = "none";
+            }
+        }
+    }
 }
 
 function createMarker(place) {
--- assets/javascript/todo-list.js
+++ assets/javascript/todo-list.js
@@ -1,33 +1,10 @@
-// TO-DO List scripts
+// TO DO list scripts
 
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("li");
-
-var i;
-
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("span");
-
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function () {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
 list.addEventListener('click', function (ev) {
-  if (ev.target.tagName === 'li') {
+  if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
   }
 }, false);
@@ -45,7 +22,7 @@ function newElement() {
   }
   document.getElementById("myInput").value = "";
 
-  var span = document.createElement("span");
+  var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
@@ -57,4 +34,4 @@ function newElement() {
       div.style.display = "none";
     }
   }
-};
+}
